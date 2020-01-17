b37d84027ceac5787230d84c74ccf48ab788ec1f
Pablo <ptrujillo@udlanet.ec>
2020-01-17 11:02:31 -0500

    Hemos implementado el navegador

--- javascript/main.js
+++ javascript/main.js
@@ -3,15 +3,32 @@ document.addEventListener('DOMContentLoaded', () => {
     var elem = document.querySelector('.carousel');
     var instance = M.Carousel.init(elem, 
         {
-        duration: 500
+        duration: 500,
+        padding:100,
     });
 
 
     setInterval(()=>{
-        console.log(instance.pressed);
         if(!instance.pressed){
           instance.next();
         }  
       
-    },2000);
+    },4000);
+
+    const showMenu = (toggleId, navId) => {
+        const toggle = document.getElementById(toggleId),
+                 nav = document.getElementById(navId)
+        
+        
+            toggle.addEventListener('click', () => {
+                nav.classList.toggle('show')
+            })
+    }
+    showMenu('menu-principal-toggle', 'nav-principal');
 });
+
+
+
+
+
+
